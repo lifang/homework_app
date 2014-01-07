6551fb0653caac45885f985e8a56446329420847
dingzuoqiang <530858106@qq.com>
2014-01-07 12:32:40 +0800

    asdas

--- gen/com/comdosoft/homework/R.java
+++ gen/com/comdosoft/homework/R.java
@@ -21,13 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bj=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int moren=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int reg_banjiyanzhengma=0x7f080003;
+        public static final int reg_nicheng=0x7f080001;
+        public static final int reg_touxiang=0x7f080000;
+        public static final int reg_xingming=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_registration=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,7 +42,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int banjiyanzhengma=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int nicheng=0x7f050003;
+        public static final int queren=0x7f050006;
+        public static final int xingming=0x7f050004;
     }
     public static final class style {
         /** 
