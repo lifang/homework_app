6df91975570b30ad2a23042a5a45688f43ef3d3a
dingzuoqiang <530858106@qq.com>
2014-01-08 19:04:57 +0800

    sfd

--- gen/com/comdosoft/homework/R.java
+++ gen/com/comdosoft/homework/R.java
@@ -32,84 +32,82 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-
         public static final int an=0x7f020000;
         public static final int an2=0x7f020001;
         public static final int bj=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int moren=0x7f020004;
-        public static final int shape=0x7f020005;
-        public static final int xlistview_arrow=0x7f020006;
+        public static final int jt=0x7f020004;
+        public static final int jt2=0x7f020005;
+        public static final int jzlb=0x7f020006;
+        public static final int ld=0x7f020007;
+        public static final int ld2=0x7f020008;
+        public static final int left=0x7f020009;
+        public static final int moren=0x7f02000a;
+        public static final int right=0x7f02000b;
+        public static final int sdlb=0x7f02000c;
+        public static final int shape=0x7f02000d;
+        public static final int sj=0x7f02000e;
+        public static final int tx=0x7f02000f;
+        public static final int tx2=0x7f020010;
+        public static final int xlistview_arrow=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f09001e;
-        public static final int button_fabiao=0x7f09000d;
-        public static final int class_button_all=0x7f09000f;
-        public static final int class_button_myself=0x7f090010;
-        public static final int class_ed1=0x7f09000e;
-        public static final int class_fabiao_content=0x7f09000c;
-        public static final int class_left=0x7f090009;
-        public static final int class_right=0x7f09000a;
+        public static final int action_settings=0x7f09002b;
+        public static final int button_fabiao=0x7f090010;
+        public static final int class_button_all=0x7f090012;
+        public static final int class_button_myself=0x7f090013;
+        public static final int class_ed1=0x7f090011;
+        public static final int class_fabiao_content=0x7f09000f;
+        public static final int class_left=0x7f09000c;
+        public static final int class_right=0x7f09000d;
         public static final int congxiangce=0x7f090006;
-        public static final int newwork_img=0x7f090012;
+        public static final int newwork_img=0x7f090021;
+        public static final int newwork_text=0x7f09001a;
         public static final int paizhaoshangchuan=0x7f090005;
         public static final int photolayout=0x7f090004;
         public static final int photolayout2=0x7f090007;
-        public static final int pull_refresh_list=0x7f090011;
-
-        public static final int bj=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int jt=0x7f020002;
-        public static final int jt2=0x7f020003;
-        public static final int jzlb=0x7f020004;
-        public static final int ld=0x7f020005;
-        public static final int ld2=0x7f020006;
-        public static final int left=0x7f020007;
-        public static final int moren=0x7f020008;
-        public static final int right=0x7f020009;
-        public static final int sdlb=0x7f02000a;
-        public static final int sj=0x7f02000b;
-        public static final int tx=0x7f02000c;
-        public static final int tx2=0x7f02000d;
-
-     
-  
-        public static final int newwork_text=0x7f090011;
-    
-        public static final int question_speak_content=0x7f090010;
-        public static final int question_speak_exit=0x7f09000c;
-        public static final int question_speak_img=0x7f09000f;
-        public static final int question_speak_next=0x7f09000e;
-        public static final int question_speak_title=0x7f09000d;
-
-        public static final int relativeLayout1=0x7f09000b;
-        public static final int sign_img=0x7f090014;
-
+        public static final int pull_refresh_list=0x7f090014;
+        public static final int question_speak_content=0x7f090019;
+        public static final int question_speak_exit=0x7f090015;
+        public static final int question_speak_img=0x7f090018;
+        public static final int question_speak_next=0x7f090017;
+        public static final int question_speak_title=0x7f090016;
+        public static final int reg_banjiyanzhengma=0x7f090003;
+        public static final int reg_nicheng=0x7f090001;
+        public static final int reg_touxiang=0x7f090000;
+        public static final int reg_xingming=0x7f090002;
+        public static final int relativeLayout1=0x7f09000e;
+        public static final int sign_img=0x7f09001c;
         public static final int title=0x7f09000a;
         public static final int work_content_img=0x7f090009;
-        public static final int work_question_case=0x7f090015;
-        public static final int work_question_end=0x7f090017;
-        public static final int work_question_img=0x7f090014;
-        public static final int work_question_time=0x7f090016;
-        public static final int work_start_date=0x7f090012;
+        public static final int work_question_case=0x7f09001e;
+        public static final int work_question_end=0x7f090020;
+        public static final int work_question_img=0x7f09001d;
+        public static final int work_question_time=0x7f09001f;
+        public static final int work_start_date=0x7f09001b;
         public static final int working_content_list=0x7f09000b;
-
         public static final int working_date_list=0x7f090008;
-        public static final int xlistview_footer_content=0x7f090015;
-        public static final int xlistview_footer_hint_textview=0x7f090017;
-        public static final int xlistview_footer_progressbar=0x7f090016;
-        public static final int xlistview_header_arrow=0x7f09001c;
-        public static final int xlistview_header_content=0x7f090018;
-        public static final int xlistview_header_hint_textview=0x7f09001a;
-        public static final int xlistview_header_progressbar=0x7f09001d;
-        public static final int xlistview_header_text=0x7f090019;
-        public static final int xlistview_header_time=0x7f09001b;
+        public static final int xlistview_footer_content=0x7f090022;
+        public static final int xlistview_footer_hint_textview=0x7f090024;
+        public static final int xlistview_footer_progressbar=0x7f090023;
+        public static final int xlistview_header_arrow=0x7f090029;
+        public static final int xlistview_header_content=0x7f090025;
+        public static final int xlistview_header_hint_textview=0x7f090027;
+        public static final int xlistview_header_progressbar=0x7f09002a;
+        public static final int xlistview_header_text=0x7f090026;
+        public static final int xlistview_header_time=0x7f090028;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_registration=0x7f030001;
         public static final int activity_working=0x7f030002;
-
+        public static final int class_middle=0x7f030003;
+        public static final int question_speak=0x7f030004;
+        public static final int work_date_adapter=0x7f030005;
+        public static final int work_question_adapter=0x7f030006;
+        public static final int working_adapter=0x7f030007;
+        public static final int xlistview_footer=0x7f030008;
+        public static final int xlistview_header=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -118,26 +116,22 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int banjiyanzhengma=0x7f060005;
-
+        public static final int classCodeError=0x7f060009;
         public static final int class_button_all=0x7f06000d;
         public static final int class_button_myself=0x7f06000e;
         public static final int class_fabiao=0x7f06000c;
         public static final int class_fabiao_tishi=0x7f06000b;
-     
-
-        public static final int classCodeError=0x7f06000b;
-        public static final int fromphotostore=0x7f06000a;
-
+        public static final int fromphotostore=0x7f060008;
         public static final int hello_world=0x7f060002;
-        public static final int newwork_text=0x7f060007;
+        public static final int newwork_text=0x7f060018;
         public static final int nicheng=0x7f060003;
         public static final int queren=0x7f060006;
-        public static final int question_speak_exit=0x7f06000d;
-        public static final int question_speak_over=0x7f06000f;
-        public static final int question_speak_start=0x7f06000e;
-        public static final int reg_fanhui=0x7f06000c;
-        public static final int takephoto=0x7f060009;
-        public static final int work_content_text=0x7f060008;
+        public static final int question_speak_exit=0x7f060015;
+        public static final int question_speak_over=0x7f060017;
+        public static final int question_speak_start=0x7f060016;
+        public static final int reg_fanhui=0x7f06000a;
+        public static final int takephoto=0x7f060007;
+        public static final int work_content_text=0x7f060019;
         public static final int xingming=0x7f060004;
         public static final int xlistview_footer_hint_normal=0x7f060013;
         public static final int xlistview_footer_hint_ready=0x7f060014;
--- src/com/comdosoft/homework/class_xinxiliu.java
+++ src/com/comdosoft/homework/class_xinxiliu.java
@@ -11,7 +11,7 @@ public class class_xinxiliu extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.class_middle);
 		
 
 //		listView = (XListView) findViewById(R.id.pull_refresh_list);
