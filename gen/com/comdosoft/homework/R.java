90b0795e91ddde412fd328dbb95c723213c286e4
dingzuoqiang <530858106@qq.com>
2014-01-08 17:30:52 +0800

    sdf

--- gen/com/comdosoft/homework/R.java
+++ gen/com/comdosoft/homework/R.java
@@ -29,30 +29,55 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int bj=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int moren=0x7f020002;
+        public static final int an=0x7f020000;
+        public static final int an2=0x7f020001;
+        public static final int bj=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int moren=0x7f020004;
+        public static final int shape=0x7f020005;
+        public static final int xlistview_arrow=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
+        public static final int action_settings=0x7f09001e;
+        public static final int button_fabiao=0x7f09000d;
+        public static final int class_button_all=0x7f09000f;
+        public static final int class_button_myself=0x7f090010;
+        public static final int class_ed1=0x7f09000e;
+        public static final int class_fabiao_content=0x7f09000c;
+        public static final int class_left=0x7f090009;
+        public static final int class_right=0x7f09000a;
         public static final int congxiangce=0x7f090006;
-        public static final int newwork_img=0x7f090009;
+        public static final int newwork_img=0x7f090012;
         public static final int paizhaoshangchuan=0x7f090005;
         public static final int photolayout=0x7f090004;
         public static final int photolayout2=0x7f090007;
+        public static final int pull_refresh_list=0x7f090011;
         public static final int reg_banjiyanzhengma=0x7f090003;
         public static final int reg_nicheng=0x7f090001;
         public static final int reg_touxiang=0x7f090000;
         public static final int reg_xingming=0x7f090002;
-        public static final int sign_img=0x7f09000b;
-        public static final int work_start_date=0x7f09000a;
+        public static final int relativeLayout1=0x7f09000b;
+        public static final int sign_img=0x7f090014;
+        public static final int work_start_date=0x7f090013;
         public static final int working_date_list=0x7f090008;
+        public static final int xlistview_footer_content=0x7f090015;
+        public static final int xlistview_footer_hint_textview=0x7f090017;
+        public static final int xlistview_footer_progressbar=0x7f090016;
+        public static final int xlistview_header_arrow=0x7f09001c;
+        public static final int xlistview_header_content=0x7f090018;
+        public static final int xlistview_header_hint_textview=0x7f09001a;
+        public static final int xlistview_header_progressbar=0x7f09001d;
+        public static final int xlistview_header_text=0x7f090019;
+        public static final int xlistview_header_time=0x7f09001b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_registration=0x7f030001;
         public static final int activity_working=0x7f030002;
-        public static final int working_adapter=0x7f030003;
+        public static final int class_middle=0x7f030003;
+        public static final int working_adapter=0x7f030004;
+        public static final int xlistview_footer=0x7f030005;
+        public static final int xlistview_header=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -62,6 +87,10 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int banjiyanzhengma=0x7f060005;
         public static final int classCodeError=0x7f060009;
+        public static final int class_button_all=0x7f06000d;
+        public static final int class_button_myself=0x7f06000e;
+        public static final int class_fabiao=0x7f06000c;
+        public static final int class_fabiao_tishi=0x7f06000b;
         public static final int fromphotostore=0x7f060008;
         public static final int hello_world=0x7f060002;
         public static final int nicheng=0x7f060003;
@@ -69,6 +98,12 @@ public final class R {
         public static final int reg_fanhui=0x7f06000a;
         public static final int takephoto=0x7f060007;
         public static final int xingming=0x7f060004;
+        public static final int xlistview_footer_hint_normal=0x7f060013;
+        public static final int xlistview_footer_hint_ready=0x7f060014;
+        public static final int xlistview_header_hint_loading=0x7f060011;
+        public static final int xlistview_header_hint_normal=0x7f06000f;
+        public static final int xlistview_header_hint_ready=0x7f060010;
+        public static final int xlistview_header_last_time=0x7f060012;
     }
     public static final class style {
         /** 
---
+++ src/com/comdosoft/homework/class_xinxiliu.java
@@ -0,0 +1,33 @@
+package com.comdosoft.homework;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.comdosoft.homework.pull.XListView;
+
+public class class_xinxiliu extends Activity {
+	private XListView listView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+
+//		listView = (XListView) findViewById(R.id.pull_refresh_list);
+//		listView.setPullLoadEnable(true);
+//		adapter = new MessageAdapter(this, list, R.layout.message_item,height);
+//		listView.setAdapter(adapter);
+////		listView.setPullLoadEnable(false);
+////		listView.setPullRefreshEnable(false);
+//		listView.setXListViewListener(this);
+//		mHandler = new Handler();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
