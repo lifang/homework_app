d33200d86b43a37c934318ae6234183012a077af
lifang <santhe_2004@126.com>
2014-01-13 17:27:37 +0800

    zxn

--- src/com/comdosoft/homework/tools/Urlinterface.java
+++ src/com/comdosoft/homework/tools/Urlinterface.java
@@ -3,7 +3,8 @@ package com.comdosoft.homework.tools;
 public interface Urlinterface {
 	static final String tag = "homework";
 	static final String SHARED = "HW";
-	static final String IP = "http://192.168.0.250:3008";
-	// static final String HEADIMG = IP + "/user_head_img/";
+//	static final String IP = "http://192.168.0.250:3008";
+	static final String IP = "http://192.168.0.101:3004/";
+	static final String CLASS_INFO = IP + "/api/students/get_class_info";
 
 }
