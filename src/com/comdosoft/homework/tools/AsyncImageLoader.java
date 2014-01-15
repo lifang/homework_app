c1cd99ea1fe9a33d9122580ab79a8d874242c126
gaoshi <908097505@qq.com>
2014-01-15 17:42:11 +0800

    gs

---
+++ src/com/comdosoft/homework/tools/AsyncImageLoader.java
@@ -0,0 +1,69 @@
+package com.comdosoft.homework.tools;
+
+import java.lang.ref.SoftReference;  
+import java.net.URL;  
+import java.util.HashMap;  
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;  
+import android.graphics.BitmapFactory;  
+import android.os.Handler;  
+import android.os.Message;  
+import android.util.Log;
+  
+  
+public class AsyncImageLoader{  
+    Bitmap bitmap = null;  
+    HashMap<String, SoftReference<Bitmap>> imageCache;  
+    BitmapFactory.Options opts;  
+    // 1.根据URL返回Drawable的函数  
+    // 2.工具类的核心函数,包含handler+thread  
+  
+    public AsyncImageLoader() {  
+        imageCache = new HashMap<String, SoftReference<Bitmap>>();  
+        opts = new BitmapFactory.Options();     
+        opts.inSampleSize = 0;    //这个的值压缩的倍数（2的整数倍），数值越小，压缩率越小，图片越清晰      
+    }  
+  
+    @SuppressLint("HandlerLeak")
+	public Bitmap asyncLoadImage(final String strUrl, final int i, final LoadFinishCallBack loadFinishCallBack) {  
+        Bitmap bitmap = null;  
+        //首先判断Map中是否有这种图片的缓存，若有，直接返回该图片的引用  
+        if(imageCache.containsKey(strUrl)) {  
+            SoftReference<Bitmap> softReference = imageCache.get(strUrl);  
+            bitmap = softReference.get();  
+            if(bitmap != null) {  
+                return bitmap;  
+            }  
+        }  
+          
+        final Handler handler = new Handler() {  
+            @Override  
+            public void handleMessage(Message msg) {  
+                // 回调接口  
+                loadFinishCallBack.loadFinish(strUrl, i, (Bitmap) msg.obj);  
+            }  
+        };  
+  
+        new Thread() {  
+            @Override  
+            public void run() {  
+                Bitmap bitmap = null;  
+                try {  
+                    bitmap = BitmapFactory.decodeStream(new URL(strUrl.substring(0,strUrl.length() - 1)).openConnection().getInputStream(), null, opts);   
+                } catch (Exception e) {  
+                    e.printStackTrace();  
+                }  
+                imageCache.put(strUrl, new SoftReference<Bitmap>(bitmap));//第一次拿某张图片，把该图片的引用放入缓存中  
+                Message msg = new Message();  
+                msg.obj = bitmap;  
+                handler.sendMessage(msg);  
+            }  
+        }.start();  
+  
+        return bitmap;  
+    }  
+  
+    public interface LoadFinishCallBack {  
+        public void loadFinish(String strUrl, int i, Bitmap bitmap);  
+    }  
+}  
--- src/com/comdosoft/homework/tools/Urlinterface.java
+++ src/com/comdosoft/homework/tools/Urlinterface.java
@@ -7,6 +7,7 @@ public interface Urlinterface {
 	static final String SHARED = "HW";
 
 	static final String IP = "http://192.168.2.55:3004";
+	static final String IP2="http://192.168.0.130:3000/";
 	// static final String HEADIMG = IP + "/user_head_img/";
 	static final String NEWS_RELEASE = IP + "/api/students/news_release";  //  发表消息
 	 String UPLOAD_FACE = IP + "/api/students/upload_avatar";  //  上传头像
@@ -15,6 +16,6 @@ public interface Urlinterface {
 	 String get_class_info = IP + "/api/students/get_class_info";  //  获得  班级信息
 	 String reply_message = IP + "/api/students/reply_message";  //  回复  获得  班级信息
 	 
-	 
+	 String get_class=IP2+"/api/students/get_my_classes";		//获得所有班级
 
 }
