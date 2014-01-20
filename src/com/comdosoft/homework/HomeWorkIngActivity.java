6f67d9eb495689a486d6a628997e480f65cd3b12
lifang <santhe_2004@126.com>
2014-01-20 17:00:45 +0800

    zxn

--- src/com/comdosoft/homework/HomeWorkIngActivity.java
+++ src/com/comdosoft/homework/HomeWorkIngActivity.java
@@ -30,14 +30,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.comdosoft.homework.pojo.ListeningPojo;
-import com.comdosoft.homework.pojo.QuestionCasePojo;
 import com.comdosoft.homework.pojo.QuestionPojo;
-import com.comdosoft.homework.pojo.WorkDatePojo;
 import com.comdosoft.homework.pojo.WorkPojo;
 import com.comdosoft.homework.tools.HomeWork;
 import com.comdosoft.homework.tools.HomeWorkParams;
 import com.comdosoft.homework.tools.HomeWorkTool;
-import com.comdosoft.homework.tools.ListeningQuestionList;
 import com.comdosoft.homework.tools.Urlinterface;
 
 public class HomeWorkIngActivity extends Activity implements Urlinterface {
